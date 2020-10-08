753e773b1ad13a4ba4d126ada1ee748d61748774
Napat <napat.wongchr@gmail.com>
2020-10-08 10:06:15 +0700

    handle go back from exercise pages

--- src/components/Instruction/index.js
+++ src/components/Instruction/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 import "./index.css";
 
 function Instruction(props) {
+  const history = useHistory();
+
+  function handleGoBackHome() {
+    history.push("/");
+  }
+
   return (
     <div className="instruction-container">
+      <div className="instruction-go-home-button" onClick={handleGoBackHome}>
+        Home
+      </div>
       <h1 className="instruction-heading">{props.topic}</h1>
       <p className="instruction-description">{props.description}</p>
     </div>
